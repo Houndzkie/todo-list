88174e171f5cefd93a91ebecb426f96f7122df05
Houndzkie <helge.justine.dano@gmail.com>
2025-06-10 18:43:04 +0800

    fixed code but still buggy

--- scripts/operations.js
+++ scripts/operations.js
@@ -1,5 +1,41 @@
-let edit = false;
+let editMode = false;
 let currentTask = null;
+
+function closePopup() {
+  let cancel = document.querySelector('.cancel-btn');
+
+  cancel.addEventListener('click', () => {
+    let hidePopup = document.querySelector('.popup-container');
+    hidePopup.style.display = 'none';
+  })
+}
+
+function showPopup(mode, taskElement = null) {
+  let popupScreen = document.querySelector('.popup-container');
+  let popupTitle = document.querySelector('.popup-title');
+  let form = document.querySelector('.task-form');
+
+  editMode = mode === 'edit';
+  currentTask = taskElement;
+
+  if (editMode) {
+    popupTitle.textContent = "Edit Task";
+    let title = taskElement.querySelector('#taskTitle').value;
+    let description = taskElement.querySelector('#taskDescription').value;
+    let start = formatTime(taskElement.querySelector('#start-time').value);
+    let end = formatTime(taskElement.querySelector('#end-time').value);
+
+    document.querySelector('#taskTitle').value = title;
+    document.querySelector('#taskDescription').value = description;
+    document.querySelector('#start-time').value = 'africa';
+    document.querySelector('#end-time').value = 'india';
+  } else {
+    popupTitle.textContent = "Add New Task";
+
+  }
+
+  closePopup();
+}
 
 
 function formatTime(time) {
@@ -13,19 +49,10 @@ function showTask(query, str) {
   let popup = document.querySelector(query);
 
   popup.addEventListener('click', () => {
-    let showPopup = document.querySelector('.popup-container');
-    showPopup.style.display = 'flex';
+    let showPopu = document.querySelector('.popup-container');
+    showPopu.style.display = 'flex';
     let title = document.querySelector('.popup-title');
     title.innerHTML = str;
-  })
-}
-
-function cancelTask() {
-  let cancel = document.querySelector('.cancel-btn');
-
-  cancel.addEventListener('click', () => {
-    let hidePopup = document.querySelector('.popup-container');
-    hidePopup.style.display = 'none';
   })
 }
 
